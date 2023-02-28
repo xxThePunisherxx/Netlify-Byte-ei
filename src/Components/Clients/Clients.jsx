f81970dff3bb1142791bfa842c3c92f20bf571a2
Nikesh <nikeshkhatri763@gmail.com>
2023-02-28 15:46:58 +0545

    Featue: Placement Partner Page

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import style from "./Clients.module.css";
 import { Link } from "react-router-dom";
-import { ClientsData } from "../../Data/clients";
+// import { ClientsData } from "../../Data/clients";
+import useFetch from "../../Utils/Hooks/fetch";
 
 const Clients = () => {
+	const { data: ClientsData, ispending } = useFetch("https://learning-management-system-kx6y.onrender.com/api/partner");
+	console.log(ClientsData);
 	return (
 		<div className={style.ClientsContainer}>
 			<h1>
-				Our <span className={style.HeadingHighlight}>Esteemed Clients</span>
+				Our <span className={style.HeadingHighlight}>Placement Partners</span>
 			</h1>
-			<div className={style.ImagesGrid}>
-				{ClientsData.map((item) => (
-					<img key={item.link} src={item.link} alt={item.link} />
-				))}
-			</div>
+			{!ispending && (
+				<div className={style.ImagesGrid}>
+					{ClientsData.partner.slice(0, 5).map((item) => (
+						<a href={item.companyWebsite}>
+							<img key={item._id} src={item.image} alt={item.link} />
+						</a>
+					))}
+				</div>
+			)}
 			<Link to={"/allClients"}>
 				<button>View More</button>
 			</Link>
--- src/Pages/Clients/ClientsAll.jsx
+++ src/Pages/Clients/ClientsAll.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import style from "./ClientsAll.module.css";
+import useFetch from "../../Utils/Hooks/fetch";
 
 const ClientsAll = () => {
+	const { data: ClientsData, ispending } = useFetch("https://learning-management-system-kx6y.onrender.com/api/partner");
+	console.log(ClientsData);
 	return (
-		<div>
-			<h1>List of all our clients.</h1>
+		<div className={style.ClientsContainer}>
+			<h1>
+				Our <span className={style.HeadingHighlight}>Placement Partners</span>
+			</h1>
+			{!ispending && (
+				<div className={style.ImagesGrid}>
+					{ClientsData.partner.map((item) => (
+						<a href={item.companyWebsite}>
+							<img key={item._id} src={item.image} alt={item.link} />
+						</a>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
