b118d9faa63b3bbbd0f492a8f8187c5a5e30ff98
Nikesh <nikeshkhatri763@gmail.com>
2023-02-28 15:46:09 +0545

    Featue: New Components on AdminDashboard

---
+++ src/Components/AdminStudentProjectList/AdminStudentProjectList.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import style from "./AdminStudentProjectList.module.css";
+import useFetch from "../../Utils/Hooks/fetch";
+import uuid from "react-uuid";
+import { MdModeEditOutline, MdDeleteSweep, MdOutlineClose } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+import MessageBoard from "../../Components/Message Board/MessageBoard";
+
+const AdminStudentProjectList = () => {
+	const { auth } = useAuth();
+	const dummyArr = [0, 1, 2, 3]; // just for adding skeleton.
+	const [ShowconfirmDelete, setShowconfirmDelete] = useState(false);
+	const [ToDelete, setToDelete] = useState(false);
+	const [showSuccecss, setshowSuccecss] = useState(false);
+	const [showFail, setShowFail] = useState(false);
+	const { data: ProjectData, ispending } = useFetch("https://learning-management-system-kx6y.onrender.com/api/project");
+	let ProjectDataArr = ProjectData?.studentProjects;
+
+	const handleDeletePopup = (id) => {
+		// show popup for confirming delete.
+		setShowconfirmDelete(true);
+		setToDelete(id);
+	};
+	const handleCancel = () => {
+		// hide popup for delete confirmation.
+		setShowconfirmDelete(false);
+	};
+	const handleConfirm = async () => {
+		try {
+			let response = await axios.delete("https://learning-management-system-kx6y.onrender.com/api/project/delete/" + ToDelete, {
+				headers: {
+					Authorization: `Bearer ${auth.Token}`,
+					withCredentails: true,
+				},
+			});
+			if (response.status === 201) {
+				setTimeout(() => {
+					setshowSuccecss(true);
+					setTimeout(() => {
+						setshowSuccecss(false);
+					}, 1000);
+					window.location.reload();
+				}, 1000);
+			}
+		} catch (error) {
+			setShowFail(true);
+			setTimeout(() => {
+				setShowFail(false);
+			}, 1000);
+		}
+	};
+	return (
+		<>
+			<div className={style.Testomonial_wrapper}>
+				<div className={style.Testomonial}>
+					<h1>
+						Students
+						<span className={style.Heading_hightlight}> Projects</span>
+					</h1>
+					<div className={style.allCourseGrid}>
+						{ispending && (
+							<div className={style.TrainingGrid}>
+								{dummyArr.map(() => (
+									<div key={uuid()} className={style.Training}>
+										<div className={style.Skel}>
+											<div className={style.imgDiv}></div>
+											<div className={style.H1Div}></div>
+											<div className={style.H2Div}></div>
+										</div>
+									</div>
+								))}
+							</div>
+						)}
+						{!ispending &&
+							ProjectDataArr.slice(0, 8).map((Team) => (
+								<div key={uuid()} className={style.AdminCourseCard}>
+									<div className={style.AdminCourseCard_Info}>
+										<img src={Team.image} alt={Team.title}></img>
+										<h1>{Team.title}</h1>
+										<div className={style.Text_Ellipse}>
+											<div dangerouslySetInnerHTML={{ __html: Team.description }}></div>
+										</div>
+									</div>
+									<div className={style.AdminCourseCard_Btn}>
+										<Link to={`/admin/updateStudentProject/${Team._id}`}>
+											<button className={style.Edit_Btn}>
+												<MdModeEditOutline />
+											</button>
+										</Link>
+										<button className={style.Delete_Btn} onClick={() => handleDeletePopup(Team._id)}>
+											<MdDeleteSweep />
+										</button>
+									</div>
+								</div>
+							))}
+					</div>
+					{!ispending && (
+						<Link to={"/admin/allTeam"}>
+							<button className={style.new}>View All</button>
+						</Link>
+					)}
+				</div>
+			</div>
+			{ShowconfirmDelete && (
+				<div className={style.popup}>
+					<div className={style.close_btn}>
+						<h1>
+							<button onClick={handleCancel}>
+								<MdOutlineClose />
+							</button>
+						</h1>
+					</div>
+					<div className={style.contnets}>
+						<h1>Are you sure you want to proceed?</h1>
+						<h2> This action cannot be reversed.</h2>
+					</div>
+					<div className={style.button_grid}>
+						<button className={style.delete} onClick={handleConfirm}>
+							Delete
+						</button>
+						<button className={style.cancel} onClick={handleCancel}>
+							Cancel
+						</button>
+					</div>
+				</div>
+			)}
+			{showSuccecss && <MessageBoard Message_type="successBoard" Message="Deleted Succesfully" />}
+			{showFail && <MessageBoard Message_type="FailedBoard" Message="Something went wrong. Please try again." />}
+		</>
+	);
+};
+
+export default AdminStudentProjectList;
