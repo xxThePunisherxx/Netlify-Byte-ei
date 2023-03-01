8c99a8729c9302ce294ea1a3b7477d23cbd57a17
Nikesh <nikeshkhatri763@gmail.com>
2023-03-01 16:22:16 +0545

    Feature: Connected Form data with backend

--- src/Pages/Contact us/Contact.jsx
+++ src/Pages/Contact us/Contact.jsx
@@ -1,86 +1,89 @@
 import React from "react";
-import { useRef, useEffect, useState } from "react";
-
+import { useRef, useEffect } from "react";
 import style from "./Contact.module.css";
+import axios from "axios";
+import { useState } from "react";
 
 const Contact = () => {
 	const EnrollRef = useRef();
+	const [Error, setError] = useState(false);
+	const [Success, setSuccess] = useState(false);
 
-	const [windowDimenion, detectHW] = useState({
-		winWidth: window.innerWidth,
-		winHeight: window.innerHeight,
-	});
-	const detectSize = () => {
-		detectHW({
-			winWidth: window.innerWidth,
-			winHeight: window.innerHeight,
-		});
-	};
-
-	useEffect(() => {
-		window.addEventListener("resize", detectSize);
-
-		return () => {
-			window.removeEventListener("resize", detectSize);
-		};
-	}, [windowDimenion]);
-
-	const handlesubmit = (e) => {
+	const handlesubmit = async (e) => {
 		e.preventDefault();
 		const data = new FormData(e.target);
 		let enterdData = Object.fromEntries(data.entries());
-
-		console.log(enterdData);
+		const postData = {
+			legalName: enterdData.Full_name,
+			email: enterdData.Email,
+			academicLevel: enterdData.Academic_level,
+			phoneNumber: enterdData.mobile_number,
+			message: enterdData.Message,
+		};
+		try {
+			const response = await axios.post("https://learning-management-system-kx6y.onrender.com/api/feedback/add", postData);
+			if (response.status === 201) {
+				// console.log("Success");
+				setSuccess(true);
+			}
+		} catch (err) {
+			setError(true);
+			setTimeout(() => {
+				setError(false);
+			}, 1000);
+		}
 	};
 	useEffect(() => {
 		EnrollRef.current.focus();
 	}, []);
 
 	return (
-		<div className={style.Contact_Wrapper}>
-			<div className={style.message_Wrapper}>
-				<div className={style.message_heading}>
-					<h1>
-						We are eager to hear <br />
-						<span className={style.HeadingHighlight}> from you.</span>
-					</h1>
-					<h2>We want to hear form you. Let us know how we can help.</h2>
-				</div>
-				<form onSubmit={handlesubmit} autoComplete="off" className={style.Enrorll_FormWrappper}>
-					<h1>
-						Legal Name
-						<span className={style.mandatory_hightlight}>
-							<sup>*&nbsp;</sup>
-						</span>
-					</h1>
-					<input name="Full name" type="text" required ref={EnrollRef}></input>
-					<h1>
-						Email
-						<span className={style.mandatory_hightlight}>
-							<sup>*&nbsp;</sup>
-						</span>
-					</h1>
-					<input name="Email" type="email" required></input>
-					<h1>
-						Mobile
-						<span className={style.mandatory_hightlight}>
-							<sup>*&nbsp;</sup>
-						</span>
-					</h1>
-					<input name="mobile number" type="text" required></input>
+		<>
+			{!Success && (
+				<div className={style.Contact_Wrapper}>
+					<div className={style.message_Wrapper}>
+						<div className={style.message_heading}>
+							<h1>
+								We are eager to hear <br />
+								<span className={style.HeadingHighlight}> from you.</span>
+							</h1>
+							<h2>We want to hear form you. Let us know how we can help.</h2>
+						</div>
+						<form onSubmit={handlesubmit} autoComplete="off" className={style.Enrorll_FormWrappper}>
+							<h1>
+								Legal Name
+								<span className={style.mandatory_hightlight}>
+									<sup>*&nbsp;</sup>
+								</span>
+							</h1>
+							<input name="Full_name" type="text" required ref={EnrollRef}></input>
+							<h1>
+								Email
+								<span className={style.mandatory_hightlight}>
+									<sup>*&nbsp;</sup>
+								</span>
+							</h1>
+							<input name="Email" type="email" required></input>
+							<h1>
+								Mobile
+								<span className={style.mandatory_hightlight}>
+									<sup>*&nbsp;</sup>
+								</span>
+							</h1>
+							<input name="mobile_number" type="text" required></input>
 
-					<h1>
-						Message
-						<span className={style.mandatory_hightlight}>
-							<sup>*&nbsp;</sup>
-						</span>
-					</h1>
-					<textarea name="Message" type="text" rows={10} required></textarea>
-					<button className={style.Spantwo}>Submit</button>
-				</form>
-			</div>
+							<h1>
+								Message
+								<span className={style.mandatory_hightlight}>
+									<sup>*&nbsp;</sup>
+								</span>
+							</h1>
+							<textarea name="Message" type="text" rows={10} required></textarea>
+							<button className={style.Spantwo}>Submit</button>
+						</form>
+					</div>
 
-			{/* <div className={style.FAQ_Wrapper}>
+					{/* <div className={style.FAQ_Wrapper}>
 				<div className={style.FAQ_header}>
 					<h1>
 						Some <span className={style.HeadingHighlight}>FAQs</span>
@@ -116,7 +119,15 @@ const Contact = () => {
 					</div>
 				</div>
 			</div> */}
-		</div>
+				</div>
+			)}
+			{Success && <h1>Succesfully Submitted</h1>}
+			{Error && (
+				<div className={style.err}>
+					<h1>Something went wrong</h1>
+				</div>
+			)}
+		</>
 	);
 };
 
